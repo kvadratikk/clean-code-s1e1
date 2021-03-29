b9ecfcfcb8f2a6442ec4813aca864c36cfdda4a3
kvadratikk <adahiter@gmail.com>
2021-03-29 22:25:15 +0300

    refactor: adjust tag selectors according to rule 3.4

--- app.js
+++ app.js
@@ -44,6 +44,7 @@ var createNewTaskElement=function(taskString){
     editButton.className="edit";
 
     deleteButton.className="delete";
+    deleteButtonImg.className="delete-img"
     deleteButtonImg.src='./remove.svg';
     deleteButton.appendChild(deleteButtonImg);
 
